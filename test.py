6be15098fb02fc971f7a6943593e4228d8bbf564
JeonHanSem <hansem@jeonhansem-ui-MacBook-Air.local>
2015-01-23 13:52:27 +0900

    testcase

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+import hangman
+
+class hangmanTestCase(unittest.TestCase):
+	def test_True(self):
+		ret = hangman.checkCorrectAnswer("tac","cat")
+		self.assertTrue(ref)
+	def test_False(self):
+		ret = hangman.checkCorrectAnswer("zebrio","zebra")
+		self(.asserTrue(answer)
+
+if __name__ == "__main__":
+	unittest.main()
